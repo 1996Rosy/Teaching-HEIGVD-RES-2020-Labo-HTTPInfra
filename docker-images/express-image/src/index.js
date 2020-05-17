5a7d500a898487dfc0c3eb5247ecee8cec1e9314
Rosy-Laure Wonjamouna <laure.wonjamouna@gmail.com>
2020-05-17 15:59:08 +0200

    Task 2 : completed

--- docker-images/express-image/src/index.js
+++ docker-images/express-image/src/index.js
@@ -1,3 +1,32 @@
 var Chance = require('chance');
-var Chance = new Chance();
-console.log("Hey!! Visit " + Chance.country({ full: true }) + " cause this country is lit!!");
+var chance = new Chance();
+//console.log("Hey!! Visit " + chance.country({ full: true }) + " cause this country is lit!!");
+
+
+const express = require('express')
+const app = express()
+
+
+app.get('/', function (req, res) {
+  res.send(generateCountry())
+})
+
+app.listen(3000, function () {
+  console.log('Accepting HTTP requests on port 3000!')
+})
+
+
+function generateCountry() {
+    var countries = []
+  //  console.log("The lucky animal of " + chance.country({full:true}) + " is " +chance.animal() + " and their national dish is " + chance.word() )
+    var limit = chance.integer({min:1, max:6})
+    for(var i = 0; i < limit; i++){
+      countries.push({
+        countryName : chance.country({full:true}),
+        symbolicAnimal: chance.animal(),
+        nationalDish: chance.word()
+      })
+    }
+    console.log(countries)
+    return countries
+  }
